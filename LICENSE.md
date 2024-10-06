e90a8080941e703aa734d33a4d524fd034ba5cdc
そら <i@kotori.love>
2024-10-06 19:13:18 +0800

    feat: add restart feature

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) 2021 Dániel Tar
+Copyright (c) 2024 kokororin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,28 @@
-"use strict";
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
+import { CronJob } from 'cron';
+import pidtree = require('pidtree');
+import getProcesses from 'getprocesses';
+import pidusage = require('pidusage');
 
 let restartTsEslintTs: vscode.StatusBarItem;
 let restartTsEslintEslint: vscode.StatusBarItem;
 let restartTsEslintBoth: vscode.StatusBarItem;
+let job: CronJob;
 
-const TYPESCRIPT_EXTENSION_ID = "vscode.typescript-language-features";
-const ESLINT_EXTENSION_ID = "dbaeumer.vscode-eslint";
+const TYPESCRIPT_EXTENSION_ID = 'vscode.typescript-language-features';
+const ESLINT_EXTENSION_ID = 'dbaeumer.vscode-eslint';
 
-const RESTART_TS_SERVER_LABEL = "$(debug-restart) Restart TS";
-const RESTART_ESLINT_SERVER_LABEL = "$(debug-restart) Restart ESLint";
-const RESTART_BOTH_LABEL = "$(debug-restart) Restart Both";
-const THIS_EXT_NAME = "restart-ts-eslint-server";
-const THIS_EXT_ID = `acoreyj.${THIS_EXT_NAME}`;
+const RESTART_TS_SERVER_LABEL = '$(debug-restart) Restart TS';
+const RESTART_ESLINT_SERVER_LABEL = '$(debug-restart) Restart ESLint';
+const RESTART_BOTH_LABEL = '$(debug-restart) Restart Both';
+const THIS_EXT_NAME = 'ts-eslint-restarter';
+const THIS_EXT_ID = `kokororin.${THIS_EXT_NAME}`;
 const SUPPORTED_LANGUAGES = [
-  "javascript",
-  "javascriptreact",
-  "typescript",
-  "typescriptreact",
-  "svelte"
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'svelte'
 ];
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -78,6 +82,7 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(restartBothCommandPalette);
 
   updateStatusBarItemVisibility();
+  startCron();
   console.log(`Extension ${THIS_EXT_ID} is now active!`);
 }
 
@@ -85,12 +90,12 @@ function softRestartEslintServer() {
   const eslintExtension = vscode.extensions.getExtension(ESLINT_EXTENSION_ID);
   if (!eslintExtension || eslintExtension.isActive === false) {
     vscode.window.showErrorMessage(
-      "ESLint extension is not active or not running."
+      'ESLint extension is not active or not running.'
     );
     return;
   }
 
-  return vscode.commands.executeCommand("eslint.restart");
+  return vscode.commands.executeCommand('eslint.restart');
 }
 
 async function softRestartTsServer() {
@@ -99,12 +104,12 @@ async function softRestartTsServer() {
   );
   if (!typeScriptExtension || typeScriptExtension.isActive === false) {
     vscode.window.showErrorMessage(
-      "TypeScript extension is not active or not running."
+      'TypeScript extension is not active or not running.'
     );
     return;
   }
 
-  await vscode.commands.executeCommand("typescript.restartTsServer");
+  await vscode.commands.executeCommand('typescript.restartTsServer');
 }
 
 async function softRestartBoth() {
@@ -126,14 +131,62 @@ function updateStatusBarItemVisibility(): void {
   } else {
     restartTsEslintTs.show();
     const eslintExtension = vscode.extensions.getExtension(ESLINT_EXTENSION_ID);
-    if (eslintExtension && eslintExtension.isActive  !== false) {
-     restartTsEslintEslint.show();
-     restartTsEslintBoth.show();
+    if (eslintExtension && eslintExtension.isActive !== false) {
+      restartTsEslintEslint.show();
+      restartTsEslintBoth.show();
     }
+  }
+}
+
+function startCron() {
+  job = new CronJob(
+    '*/30 * * * * *',
+    () => {
+      checkEslintServer();
+    },
+    null,
+    false
+  );
+  job.start();
+}
+
+function stopCron() {
+  if (job) {
+    job.stop();
+  }
+}
+
+async function checkEslintServer() {
+  const eslintMaxMemory = vscode.workspace
+    .getConfiguration(THIS_EXT_NAME)
+    .get<number>('eslintMaxMemory', 3072);
+
+  const mainPid = process.pid;
+  const processes = await getProcesses();
+  const childPids = await pidtree(mainPid);
+
+  const eslintProcess = processes.find(p => {
+    return (
+      p.rawCommandLine.includes(ESLINT_EXTENSION_ID) &&
+      childPids.includes(p.pid)
+    );
+  });
+  if (!eslintProcess) {
+    return;
+  }
+  const stats = await pidusage(eslintProcess.pid);
+  const memMb = Math.round(stats.memory / 1024 / 1024);
+
+  if (memMb > eslintMaxMemory) {
+    console.log(
+      `Memory usage of eslint server is over ${eslintMaxMemory} MB (${memMb} bytes).`
+    );
+    await softRestartEslintServer();
   }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+  stopCron();
   console.log(`Extension ${THIS_EXT_ID} is now inactive!`);
 }
